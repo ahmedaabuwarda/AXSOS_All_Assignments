63b0a60a5519b9fc3efdefe5784e7076459ea81d
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2026-03-03 15:10:53 +0200

    solve JS practice 1 + fix some css

---
+++ Web-Fun/JS/Practice-I.js
@@ -0,0 +1,38 @@
+// JS Assignments
+// Name: Ahmed Abuwarda, Date: 03/03/2026
+
+// 1
+let favoriteAnimal = "cat";
+console.log("Favorite Animal: " + favoriteAnimal);
+
+// 2
+let a = 5;
+let b = 7;
+let sum = a + b;
+console.log("The sum of " + a + " and " + b + " is: " + sum);
+
+// 3
+let age = 27;
+console.log("I am " + age + " years old.");
+
+// 4
+let country = "Egypt";
+country = "United Arab Emirates";
+console.log("I live in " + country + ".");
+
+// 5
+const pi = 3.14;
+console.log("The value of pi is: " + pi);
+
+// 6
+let favoriteColor = "blue";
+console.log("My favorite color is: " + favoriteColor);
+
+// 7
+let x = 5;
+let y = 10;
+console.log("The sum of " + x + " and " + y + " is: " + (x + y));
+
+// 8
+let isSunny = true;
+console.log("Is it sunny today? " + isSunny);
