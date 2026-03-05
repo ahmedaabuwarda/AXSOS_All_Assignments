12817651ca0448f4419a94d0ce6913a5c6bfc796
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2026-03-05 13:21:27 +0200

    solve js practice-3

---
+++ Web-Fun/JS/Practice-3/practice.js
@@ -0,0 +1,20 @@
+// Ahmed Abuwarda, Date 05/03/2026
+// Basic Function: Write a function called welcome that prints "Welcome to coding!" when called.
+function welcome() {
+    console.log("Welcome to coding!");
+}
+
+// With Arguments: Write a function called square that takes a number as input and prints its square.
+function square(num) {
+    console.log(num * num);
+}
+
+// With Return Value: Write a function called convertToCelsiusthat takes a temperature in Fahrenheit and returns the temperature in Celsius.Formula: C = (F - 32) * 5 / 9
+function convertToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+// Reusable Function: Write a function called isEven that checks if a number is even or odd and returns true or false.
+function isEven(num) {
+    return num % 2 === 0;
+}
