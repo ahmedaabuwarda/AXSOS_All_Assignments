2f0dd2f119fa216b2cdcb36851a40e0fa169e31f
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2026-04-11 12:49:21 +0300

    solve 3rd python HWs

---
+++ python_stack/_python/python_fundamentals/hello_world.py
@@ -0,0 +1,20 @@
+print("Hello World!")
+name = "Ahmed"
+print("Hello", name, "!")
+print("Hello " + name + "!")
+num = 5
+print("Hello " + str(num) + "!")
+
+food_one = "Pizza"
+food_tow = "Burger"
+print("I love to eat {food_one} and {food_tow}".format(food_one=food_one, food_tow=food_tow))
+
+print(f"I love to eat {food_one} and {food_tow}")
+
+game = "new game of thrones series"
+print(game.upper()) # capitalizes all letters
+print(game.title()) # capitalizes the first letter of each word
+
+print(game.split())
+
+# print(game.join(""))
