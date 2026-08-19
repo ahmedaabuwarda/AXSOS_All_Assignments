a7736f9e337262aae67be42e22da1a1338881e47
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2026-08-19 17:29:15 +0300

    Add initial implementation of Dojo Survey and Form Test applications
    - Created Dojo Survey application with models, views, templates, and static files.
    - Implemented form handling and result display for user submissions.
    - Added CSS styles for better presentation of forms and results.
    - Established URL routing for both Dojo Survey and Form Test applications.
    - Created Form Test application with user creation functionality and success page.
    - Included necessary Django settings and configurations for both applications.

---
+++ python_stack/django/django_intro/Dojo-Survey/dojo_survey_project/dojo_survey_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def result(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        location = request.POST.get('location')
+        language = request.POST.get('language')
+        comment = request.POST.get('comment')
+
+        context = {
+            'name': name,
+            'location': location,
+            'language': language,
+            'comment': comment
+        }
+
+        return render(request, 'result.html', context)
+    
+    return render(request, 'result.html')
