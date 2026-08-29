729cb87e6e8b6beb3d5dfdf9de4247b553f3da34
Ahmed Abuwarda <ahmedaabuwarda@gmail.com>
2026-08-29 16:49:22 +0300

    Add user management features with index and create views, CSS styling, and HTML template

--- python_stack/django/django_intro/SQL_Queries_to_ORM/SQL_Queries_to_ORM_project/SQL_Queries_to_ORM_app/views.py
+++ python_stack/django/django_intro/SQL_Queries_to_ORM/SQL_Queries_to_ORM_project/SQL_Queries_to_ORM_app/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, redirect
+from .models import users
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+
+    context = {
+        "all_the_users": users.objects.all()
+    }
+
+    # print(context)
+    
+    return render(request,'index.html', context)
+
+def create(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email_Address = request.POST['email_address']
+    age = request.POST['age']
+
+    time = datetime.datetime.now()
+    print(time)
+
+    users.objects.create(first_name=first_name, last_name=last_name, email_address=email_Address, age=age, created_at="2026-08-01", updated_at="2026-08-01")
+    return redirect('/')
+
